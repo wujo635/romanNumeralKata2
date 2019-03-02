762e228d2f31dd26832b45c722342bd588fb4323
wujo635 <wujo635@gmailc.om>
2019-03-02 01:37:59 -0500

    Pass convert single roman numeral from arabic

--- src/main/java/com/pillar/RomanNumeralConverter.java
+++ src/main/java/com/pillar/RomanNumeralConverter.java
@@ -2,16 +2,16 @@ package com.pillar;
 
 public class RomanNumeralConverter {
 
-    private String[] numerals = new String[]{"V", "I"};
-    private int[] values = new int[]{5, 1};
+    private String[] numerals = new String[]{"M", "D", "C", "L", "X", "V", "I"};
+    private int[] values = new int[]{1000, 500, 100, 50, 10, 5, 1};
 
     public String convertToRoman(int arabicValue) {
         String romanNumeral = "";
         int remainingToConvert = arabicValue;
         while (remainingToConvert > 0) {
             for (int index = 0; index < numerals.length; index++) {
-                int numeralCount = remainingToConvert/values[index];
-                romanNumeral += new String(new char[numeralCount]).replace("\0",numerals[index]);
+                int numeralCount = remainingToConvert / values[index];
+                romanNumeral += new String(new char[numeralCount]).replace("\0", numerals[index]);
                 remainingToConvert -= numeralCount * values[index];
             }
         }
