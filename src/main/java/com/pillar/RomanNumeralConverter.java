16bbcd73f76bcc1c985758c8a94350a6eca51266
wujo635 <wujo635@gmailc.om>
2019-03-02 01:14:29 -0500

    Pass convert 2 to II

--- src/main/java/com/pillar/RomanNumeralConverter.java
+++ src/main/java/com/pillar/RomanNumeralConverter.java
@@ -2,6 +2,6 @@ package com.pillar;
 
 public class RomanNumeralConverter {
     public String convertToRoman(int arabicValue) {
-        return "I";
+        return new String(new char[arabicValue]).replace("\0","I");
     }
 }
