f1ef37b43b343e95dab1aba963f18b9c70868f18
wujo635 <wujo635@gmailc.om>
2019-03-02 01:32:34 -0500

    Pass convert 5 to V

--- src/main/java/com/pillar/RomanNumeralConverter.java
+++ src/main/java/com/pillar/RomanNumeralConverter.java
@@ -1,7 +1,20 @@
 package com.pillar;
 
 public class RomanNumeralConverter {
+
+    private String[] numerals = new String[]{"V", "I"};
+    private int[] values = new int[]{5, 1};
+
     public String convertToRoman(int arabicValue) {
-        return new String(new char[arabicValue]).replace("\0","I");
+        String romanNumeral = "";
+        int remainingToConvert = arabicValue;
+        while (remainingToConvert > 0) {
+            for (int index = 0; index < numerals.length; index++) {
+                int numeralCount = remainingToConvert/values[index];
+                romanNumeral += new String(new char[numeralCount]).replace("\0",numerals[index]);
+                remainingToConvert -= numeralCount * values[index];
+            }
+        }
+        return romanNumeral;
     }
 }
