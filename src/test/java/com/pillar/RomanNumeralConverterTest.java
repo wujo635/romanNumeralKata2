0aad20ed04bafbe509f6b5baf04190f18ea30223
wujo635 <wujo635@gmailc.om>
2019-03-02 01:39:07 -0500

    Refactor test names

--- src/test/java/com/pillar/RomanNumeralConverterTest.java
+++ src/test/java/com/pillar/RomanNumeralConverterTest.java
@@ -9,18 +9,8 @@ public class RomanNumeralConverterTest {
     private RomanNumeralConverter converter = new RomanNumeralConverter();
 
     @Test
-    public void shouldConvert1ToI() {
-        assertEquals("I", converter.convertToRoman(1));
-    }
-
-    @Test
-    public void shouldConvert2toII() {
+    public void shouldConvertMultipleOfSameRomanNumeralFromArabicValue() {
         assertEquals("II", converter.convertToRoman(2));
-    }
-
-    @Test
-    public void shouldConvert5toV() {
-        assertEquals("V", converter.convertToRoman(5));
     }
 
     @Test
